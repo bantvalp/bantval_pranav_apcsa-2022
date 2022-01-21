96b2a685057b5df67e67facf8ed23abe3a8833d2
bantvalp <98112205+bantvalp@users.noreply.github.com>
2022-01-20 22:34:21 -0800

    Update Input.java

--- unit00/src/Input.java
+++ unit00/src/Input.java
@@ -30,11 +30,34 @@ public class Input
 
 
 		//add in input for all variables
+		System.out.print("Enter an double :: ");
+		doubleOne = keyboard.nextDouble();
+
+
+		System.out.print("Enter an double :: ");
+		doubleTwo = keyboard.nextDouble();
+		System.out.print("Enter an float :: ");
+		floatOne = keyboard.nextFloat();
+
+
+		System.out.print("Enter an float :: ");
+		floatTwo = keyboard.nextFloat();
+		System.out.print("Enter an short :: ");
+		shortOne = keyboard.nextShort();
+
+		System.out.print("Enter an short :: ");
+		shortTwo = keyboard.nextShort();
 
 
 		System.out.println();
 		System.out.println("integer one = " + intOne );
 		System.out.println("integer two = " + intTwo );
+		System.out.println("double one = " + doubleOne );
+		System.out.println("double two = " + doubleTwo );
+		System.out.println("float one = " + floatOne );
+		System.out.println("float two = " + floatTwo );
+		System.out.println("short one = " + shortOne );
+		System.out.println("short two = " + shortTwo );
 
 		//add in output for all variables
 
