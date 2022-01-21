6f3a6eaf7311ffb4debb7992b3cccb6d3cf0822b
bantvalp <98112205+bantvalp@users.noreply.github.com>
2022-01-20 22:34:36 -0800

    Update Variables.java

--- unit00/src/Variables.java
+++ unit00/src/Variables.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Pranav Bantval
+//Date - 1-20-22
+//Class - ap csa
+//Lab  - variables unit00
 
 public class Variables
 {
@@ -18,25 +18,43 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
-
-
+		short shortOne = -32123;
+		int intOne= 12121212;
+		long longOne = 999999999;
 		//decimal variables
-
-
+		
+		double doubleOne = 1231.1321;
+		float floatOne = 21.33f;
 		//other integer types
-
+		char charOne= 'B';
 
 		//other types
-
+		boolean booleanOne = true;
+		String stringOne = "hi";
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
-		System.out.println("*                               *");
-		System.out.println("*        integer types          *");
-		System.out.println("*                               *");
-		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-
-
+		System.out.println("Pranav Bantval          1/20/22");
+		System.out.println("                               ");
+		System.out.println("        integer types          ");
+		System.out.println("                               ");
+		System.out.println("8 bit - byteOne = "+byteOne+"\t\t");
+		System.out.println("16 bit - shortOne = "+shortOne+"\t\t");
+		System.out.println("32 bit - intOne = "+intOne+"\t\t");
+		System.out.println("64 bit - longOne = "+longOne+"\t\t");
+		System.out.println("                               ");
+		System.out.println("         real types            ");
+		System.out.println("32 bit - floatOne = "+floatOne+"\t\t");
+		System.out.println("64 bit - doubleOne = "+doubleOne+"\t\t");
+		System.out.println("                               ");
+		System.out.println("     other integer types       ");
+		System.out.println("16 bit - charOne =            "+charOne+"\t\t");
+		System.out.println("                               ");
+		System.out.println("         other types           ");
+		System.out.println("                               ");
+		System.out.println("booleanOne =               "+booleanOne+"\t\t");
+		System.out.println("stringOne =        "+stringOne+"\t\t");
+		System.out.println("/////////////////////////////////");
 
 
 
