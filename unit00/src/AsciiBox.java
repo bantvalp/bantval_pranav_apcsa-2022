95a0910afdce8681ddee993c285fee4d3f339549
bantvalp <98112205+bantvalp@users.noreply.github.com>
2022-01-20 22:34:08 -0800

    Update AsciiBox.java

--- unit00/src/AsciiBox.java
+++ unit00/src/AsciiBox.java
@@ -1,17 +1,18 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Pranav Bantval
+//Date - 1-20-22
+//Class - AP CSA p 1
+//Lab  -Asciibox unit00
 
-public class AsciiBox
-{
-	public static void main(String[] args)
-	{
-		System.out.println("name \t  date \n\n" );
-		System.out.println("+++++++++++++++++++++++++ " );
+public class AsciiBox {
+	public static void main(String[] args) {
+		System.out.println("name \t  date \n\n");
+		System.out.println("____________\n" + 
+		                   "|          |\n"
+		                  +"|          |\n"+
+		                   "|__________|");
 
 	}
 }
